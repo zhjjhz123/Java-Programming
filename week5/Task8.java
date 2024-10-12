0aa72cb1a15a2ce885116dcb2b76344d35395221
zhjjhz123 <2045783557@qq.com>
2024-10-12 11:27:59 +0800

    Delete week5 directory

--- week5/Task8.java
+++
@@ -1,6 +0,0 @@
-public class Task8 {
-    public static void main(String[] args) {
-        String message = "Welcome to Java";
-        System.out.println("The length of \"" + message + "\" is " + message.length());
-    }
-}
--- week5/Task9.java
+++
@@ -1,6 +0,0 @@
-public class Task9 {
-    public static void main(String[] args) {
-        String message = "Welcome to Java";
-        System.out.println("The first character in message is " + message.charAt(0));
-    }
-}
