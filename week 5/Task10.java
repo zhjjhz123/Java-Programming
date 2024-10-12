607b81eadcb75cdd77b9dead7cdeb24b018c5472
zhjjhz123 <2045783557@qq.com>
2024-10-12 15:39:21 +0800

    Delete week 5 directory

--- week 5/Task10.java
+++
@@ -1,7 +0,0 @@
-public class Task10 {
-    public static void main(String[] args) {
-        String message = "Welcome to Java";
-        System.out.println("The message in lowercase is: " + message.toLowerCase());
-        System.out.println("The message in UPPERCASE is: " + message.toUpperCase());
-    }
-}
--- week 5/Task20.java
+++
@@ -1,6 +0,0 @@
-public class Task20 {
-    public static void main(String[] args) {
-        System.out.printf("%8d%8s%8.1f\n", 1234, "Java", 5.63);
-        System.out.printf("%-8d%-8s%-8.1f\n", 1234, "Java", 5.63);
-    }
-}
